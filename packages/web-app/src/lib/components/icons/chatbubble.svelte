a7faac2c96516480515d7d4c4c5ce2402165e40a
JeffreyAshtonNRCan <jeffrey.ashton@nrcan-rncan.gc.ca>
2026-05-25 14:12:15 -0400

    chatbubble icon

--- packages/web-app/src/lib/components/icons/chatbubble.svelte
+++ packages/web-app/src/lib/components/icons/chatbubble.svelte
@@ -6,13 +6,8 @@
   source: https://css.gg/message
   Licence: MIT https://css.gg/doc/licence
 -->
-<svg
-  class={classes}
-  width="16"
-  height="16"
-  viewBox="0 0 20 20"
-  fill="currentColor"
-  aria-hidden="true"
->
-  <path d="M10 2C5.589 2 2 5.134 2 9c0 1.657.672 3.162 1.764 4.313L2 18l4.922-1.29A9.96 9.96 0 0010 17c4.411 0 8-3.134 8-7s-3.589-8-8-8zm-3 5h6v1H7V7zm0 3h4v1H7v-1z"/>
+<svg class={classes} viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+  <path
+    d="M10 2C5.589 2 2 5.134 2 9c0 1.657.672 3.162 1.764 4.313L2 18l4.922-1.29A9.96 9.96 0 0010 17c4.411 0 8-3.134 8-7s-3.589-8-8-8zm-3 5h6v1H7V7zm0 3h4v1H7v-1z"
+  />
 </svg>
