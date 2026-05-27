5d37a6477400ae1a54cb51e6f70d68aafc4fe6d3
JeffreyAshtonNRCan <jeffrey.ashton@nrcan-rncan.gc.ca>
2026-05-27 09:17:50 -0400

    icons

--- packages/web-app/src/lib/geochat/components/chat-panel.svelte
+++ packages/web-app/src/lib/geochat/components/chat-panel.svelte
@@ -53,7 +53,7 @@
         <button class="chat-expand" onclick={() => (isExpanded = !isExpanded)}> ⛶ </button>
 
         <button class="chat-close" onclick={() => (isOpen = false)}>
-          <CloseIcon classes="h-3 w-3 md:h-4 md:w-4" />
+          <CloseIcon classes="h-3 w-3 md:h-3 md:w-3" />
         </button>
       </div>
     </div>
