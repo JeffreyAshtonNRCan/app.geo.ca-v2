34c814e8e426aecdc451581c6d4d5751bac30b60
JeffreyAshtonNRCan <jeffrey.ashton@nrcan-rncan.gc.ca>
2026-06-08 10:27:14 -0400

    draggable 2

--- packages/web-app/src/lib/geochat/components/chat-panel.svelte
+++ packages/web-app/src/lib/geochat/components/chat-panel.svelte
@@ -157,7 +157,16 @@
           class="chat-expand"
           aria-label="Large chat"
           title={isExpanded ? 'Small Chat' : 'Large Chat'}
-          onclick={() => (isExpanded = !isExpanded)}
+          onclick={() => {
+            isExpanded = !isExpanded;
+
+            if (chatbotPanel) {
+              chatbotPanel.style.left = '';
+              chatbotPanel.style.top = '';
+              chatbotPanel.style.right = '';
+              chatbotPanel.style.bottom = '';
+            }
+          }}
         >
           <ExpandIcon classes="h-4 w-4 md:h-5 md:w-5" />
         </button>
