c321e2b21e278c02792747f55f3585967a44f25c
JeffreyAshtonNRCan <jeffrey.ashton@nrcan-rncan.gc.ca>
2026-06-17 08:28:47 -0400

    download link 2

--- packages/web-app/src/lib/geochat/components/chat-panel.svelte
+++ packages/web-app/src/lib/geochat/components/chat-panel.svelte
@@ -604,6 +604,7 @@
   .bot-text :global(a[data-download="true"])::after {
     content: '';
     display: inline-block;
+    white-space: nowrap;
     width: 1.5em;
     height: 1.5em;
     margin-left: 0.2em;
