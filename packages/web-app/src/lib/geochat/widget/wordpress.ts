197a1ac7488c60dfff1daf6ed1fc123dcf564375
JeffreyAshtonNRCan <jeffrey.ashton@nrcan-rncan.gc.ca>
2026-06-23 14:41:44 -0400

    wordpress mount

--- packages/web-app/src/lib/geochat/widget/wordpress.ts
+++ packages/web-app/src/lib/geochat/widget/wordpress.ts
@@ -3,7 +3,11 @@ import WordPressWidget from './wordpress-widget.svelte';
 
 const target = document.getElementById('geochat-root');
 
+console.log('target', target);
+
 if (target) {
+  console.log('mounting widget');
+
   mount(WordPressWidget, {
     target,
   });
