3c15fc8f27f79f303cd6f8e42f12df713d40b1c6
JeffreyAshtonNRCan <jeffrey.ashton@nrcan-rncan.gc.ca>
2026-06-23 08:39:51 -0400

    dive deeper remove slot

--- packages/web-app/src/lib/geochat/widget/geochat-widget.svelte
+++ packages/web-app/src/lib/geochat/widget/geochat-widget.svelte
@@ -16,15 +16,6 @@
     alternateLanguageUrl?: string;
   } = $props();
 
-  const UI_TEXT = {
-    en: {
-      diveDeeper: 'Dive deeper with GeoChat',
-    },
-    fr: {
-      diveDeeper: 'Approfondissez avec GéoChat',
-    },
-  } as const;
-
   let chatbotPanel = $state<HTMLDivElement | undefined>(undefined);
 
   let isOpen = $state(false);
@@ -46,10 +37,6 @@
     if (chatbotPanel) {
       chatbotPanel.removeAttribute('style');
     }
-  }
-
-  function openFullGeoChat() {
-    window.location.href = lang === 'fr' ? '/fr-ca/geochat' : '/en-ca/geochat';
   }
 
   $effect(() => {
@@ -108,14 +95,7 @@
         </button>
       </div>
     </div>
-    <ChatPanel {lang} {alternateLanguageUrl}>
-      <div slot="actions" id="chat-actions">
-        <button class="dive-deeper-button" onclick={openFullGeoChat} disabled>
-          <ChatBubbleIcon />
-          {UI_TEXT[lang].diveDeeper}
-        </button>
-      </div>
-    </ChatPanel>
+    <ChatPanel {lang} {alternateLanguageUrl} showDiveDeeper={true} />
   </div>
 {/if}
 
