9b13b735f309939c5be4225af83a6021e243693a
JeffreyAshtonNRCan <jeffrey.ashton@nrcan-rncan.gc.ca>
2026-07-03 14:44:40 -0400

    chat panel 2

--- packages/web-app/src/lib/geochat/components/geochat-page.svelte
+++ packages/web-app/src/lib/geochat/components/geochat-page.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-  import HistoryPanelPanel from '$lib/geochat/components/history-panel.svelte';
+  import HistoryPanel from '$lib/geochat/components/history-panel.svelte';
   import ChatPanel from '$lib/geochat/components/chat-panel.svelte';
   import RecordsPanel from '$lib/geochat/components/records-panel.svelte';
 </script>
 
 <div class="geochat-page">
   <aside class="history">
-    <HistoryPanelPanel />
+    <HistoryPanel />
   </aside>
 
   <div class="chat">
@@ -23,8 +23,11 @@
 <style>
   .geochat-page {
     display: grid;
-    grid-template-columns: 320px 1fr 380px;
+    grid-template-columns: 320px minmax(0, 1fr) 380px;
     gap: 1rem;
+
+    width: 100%;
+    min-width: 0;
 
     height: calc(100vh - 180px);
   }
@@ -38,11 +41,12 @@
 
   .chat {
     display: flex;
+    width: 100%;
   }
 
   .chat-panel {
     display: flex;
-    flex: 1;
+    flex: 1 1 auto;
 
     min-width: 0;
     min-height: 0;
@@ -51,7 +55,7 @@
   }
 
   .chat-panel > * {
-    flex: 1;
+    flex: 1 1 auto;
     min-width: 0;
     min-height: 0;
   }
