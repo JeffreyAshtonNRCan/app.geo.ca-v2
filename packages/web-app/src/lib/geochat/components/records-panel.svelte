c62d1b0677f0a74f6fb9c628c26dd9862ca216e2
JeffreyAshtonNRCan <jeffrey.ashton@nrcan-rncan.gc.ca>
2026-07-06 09:19:25 -0400

    records

--- packages/web-app/src/lib/geochat/components/records-panel.svelte
+++ packages/web-app/src/lib/geochat/components/records-panel.svelte
@@ -1,15 +1,18 @@
-<h3>Records & Map</h3>
+<div class="records-panel">
+  <h3>Records & Map</h3>
 
-<div class="records-body">
-  <div class="records-list">Supporting records will appear here.</div>
+  <div class="records-body">
+    <div class="records-list">Supporting records will appear here.</div>
 
-  <div class="map">Map placeholder</div>
+    <div class="map">Map placeholder</div>
+  </div>
 </div>
 
 <style>
   :global(.records) {
     display: flex;
     flex-direction: column;
+    min-height: 0;
   }
 
   h3 {
@@ -18,25 +21,29 @@
     border-bottom: 1px solid #ddd;
   }
 
-  .records-body {
+  .records-panel {
     display: flex;
     flex-direction: column;
+    height: 100%;
+    min-height: 0;
+  }
+
+  .records-body {
+    display: grid;
+    grid-template-rows: 1fr 1fr;
     gap: 1rem;
     padding: 1rem;
     flex: 1;
+    min-height: 0;
   }
 
-  .records-list {
-    min-height: 150px;
+  .records-list,
+  .map {
+    min-height: 0;
     border: 1px solid #ddd;
-    padding: 1rem;
   }
 
   .map {
-    flex: 1;
-    min-height: 250px;
-    border: 1px solid #ddd;
-
     display: flex;
     align-items: center;
     justify-content: center;
