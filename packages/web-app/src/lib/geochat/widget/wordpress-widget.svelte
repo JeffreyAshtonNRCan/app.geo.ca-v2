54777e02633c7b8afb898d6b74544dc03bf36106
JeffreyAshtonNRCan <jeffrey.ashton@nrcan-rncan.gc.ca>
2026-07-08 14:14:01 -0400

    wordpress set locale

--- packages/web-app/src/lib/geochat/widget/wordpress-widget.svelte
+++ packages/web-app/src/lib/geochat/widget/wordpress-widget.svelte
@@ -2,10 +2,10 @@
   import GeoChatWidget from './geochat-widget.svelte';
   import { getWordPressLang } from './wordpress-utils';
 
-  const { lang, alternateLanguageUrl } = getWordPressLang();
+  const { lang, locale, alternateLanguageUrl } = getWordPressLang();
 
   function handleDiveDeeper() {
-    window.location.href = `https://app.geo.ca/${lang}/geochat`;
+    window.location.href = `https://app.geo.ca/${locale}/geochat`;
   }
 </script>
 
