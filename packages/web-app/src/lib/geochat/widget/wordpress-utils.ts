354d22c58b151c53d911cdf6270339ecaa6bb325
JeffreyAshtonNRCan <jeffrey.ashton@nrcan-rncan.gc.ca>
2026-07-08 14:14:01 -0400

    wordpress set locale

--- packages/web-app/src/lib/geochat/widget/wordpress-utils.ts
+++ packages/web-app/src/lib/geochat/widget/wordpress-utils.ts
@@ -1,13 +1,15 @@
 // src/lib/geochat/widget/wordpress-utils.ts
 
 export function getWordPressLang() {
-  let lang: 'en-ca' | 'fr-ca' = 'en-ca';
+  let lang: 'en' | 'fr' = 'en';
+  let locale: 'en-ca' | 'fr-ca' = 'en-ca';
   let alternateLanguageUrl = '';
 
   const path = window.location.pathname;
 
   if (path.startsWith('/fr/')) {
-    lang = 'fr-ca';
+    lang = 'fr';
+    locale = 'fr-ca';
   }
 
   const links = document.querySelectorAll('link[rel="alternate"][hreflang]');
@@ -16,13 +18,14 @@ export function getWordPressLang() {
     const hreflang = link.getAttribute('hreflang');
     const href = link.getAttribute('href');
 
-    if ((lang === 'en-ca' && hreflang === 'fr') || (lang === 'fr-ca' && hreflang === 'en')) {
+    if ((lang === 'en' && hreflang === 'fr') || (lang === 'fr' && hreflang === 'en')) {
       alternateLanguageUrl = href ?? '';
     }
   });
 
   return {
     lang,
+    locale,
     alternateLanguageUrl,
   };
 }
