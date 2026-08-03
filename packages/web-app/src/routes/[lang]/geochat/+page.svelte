66aebb68df6c3207ddc05a1bdb4d2312f731d071
JeffreyAshtonNRCan <jeffrey.ashton@nrcan-rncan.gc.ca>
2026-08-03 09:47:40 -0400

    +page lang

--- packages/web-app/src/routes/[lang]/geochat/+page.svelte
+++ packages/web-app/src/routes/[lang]/geochat/+page.svelte
@@ -2,7 +2,9 @@
   import GeoChatPage from '$lib/geochat/components/geochat-page.svelte';
   import { page } from '$app/state';
 
-  const lang = page.params.lang === 'fr' ? 'fr' : 'en';
+  console.log('page.params.lang =', page.params.lang);
+
+  const lang: 'en' | 'fr' = page.params.lang?.startsWith('fr') ? 'fr' : 'en';
 </script>
 
 <GeoChatPage {lang} />
