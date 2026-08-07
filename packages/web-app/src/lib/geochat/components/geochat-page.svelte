4845d32bbac5858953cb62d99101c64ae41f8502
JeffreyAshtonNRCan <jeffrey.ashton@nrcan-rncan.gc.ca>
2026-08-07 11:49:42 -0400

    history info collapsed 2

--- packages/web-app/src/lib/geochat/components/geochat-page.svelte
+++ packages/web-app/src/lib/geochat/components/geochat-page.svelte
@@ -45,9 +45,9 @@
   <Info classes="w-5 h-5 mr-2" />
   <span>About chat history</span>
   {#if showHistoryInfo}
-    <ChevronUp classes="w-4 h-4" />
+    <ChevronUp classes="w-4 h-4 ml-2" />
   {:else}
-    <ChevronDown classes="w-4 h-4" />
+    <ChevronDown classes="w-4 h-4 ml-2" />
   {/if}
 </button>
 
