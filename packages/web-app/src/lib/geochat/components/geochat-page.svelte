979a7f69aa8f10ff49f41c9bb5fbc4deb780660e
JeffreyAshtonNRCan <jeffrey.ashton@nrcan-rncan.gc.ca>
2026-09-04 09:13:33 -0400

    history info section 1

--- packages/web-app/src/lib/geochat/components/geochat-page.svelte
+++ packages/web-app/src/lib/geochat/components/geochat-page.svelte
@@ -84,24 +84,26 @@
 <p class="mt-3 px-5 md:px-0 font-open-sans">{description}</p>
 
 <div class="geochat-page px-5 md:px-0" class:history-collapsed={historyCollapsed}>
-  <div class="panel history" class:collapsed={historyCollapsed}>
-    <div class="history-info-section">
-      <button class="history-info" class:collapsed={!showHistoryInfo} onclick={() => (showHistoryInfo = !showHistoryInfo)}>
-        <Info classes="w-5 h-5 mr-2" />
-        <span>{aboutChatHistory}</span>
-        {#if showHistoryInfo}
-          <ChevronUp classes="w-4 h-4 ml-2" />
-        {:else}
-          <ChevronDown classes="w-4 h-4 ml-2" />
-        {/if}
-      </button>
-
+  <!-- About Chat History: outside the panel -->
+  <div class="history-info-section" class:hidden={historyCollapsed}>
+    <button class="history-info" class:collapsed={!showHistoryInfo} onclick={() => (showHistoryInfo = !showHistoryInfo)}>
+      <Info classes="w-5 h-5 mr-2" />
+      <span>{aboutChatHistory}</span>
       {#if showHistoryInfo}
-        <p class="history-note">
-          {aboutChatHistoryText}
-        </p>
+        <ChevronUp classes="w-4 h-4 ml-2" />
+      {:else}
+        <ChevronDown classes="w-4 h-4 ml-2" />
       {/if}
-    </div>
+    </button>
+
+    {#if showHistoryInfo}
+      <p class="history-note">
+        {aboutChatHistoryText}
+      </p>
+    {/if}
+  </div>
+
+  <div class="panel history" class:collapsed={historyCollapsed}>
     <div class="panel-header history-header">
       <h2>{chatHistory}</h2>
     </div>
