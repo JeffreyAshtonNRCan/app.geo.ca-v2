bbfd9e109d6454432eeca9d437359b295466cb96
JeffreyAshtonNRCan <jeffrey.ashton@nrcan-rncan.gc.ca>
2026-09-04 13:43:51 -0400

    panel border test 6px

--- packages/web-app/src/lib/geochat/components/geochat-page.svelte
+++ packages/web-app/src/lib/geochat/components/geochat-page.svelte
@@ -298,7 +298,7 @@
   .panel {
     display: flex;
     flex-direction: column;
-    border: 2px solid #ddd;
+    border: 6px solid #ddd;
     background: #fff;
     min-height: 0;
     box-sizing: border-box;
