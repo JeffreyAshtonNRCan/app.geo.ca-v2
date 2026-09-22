5f16bb475aee65f4c55c5bf87f839cad662dd257
JeffreyAshtonNRCan <jeffrey.ashton@nrcan-rncan.gc.ca>
2026-09-22 15:33:04 -0400

    map key uuid

--- packages/web-app/src/lib/geochat/components/records-panel.svelte
+++ packages/web-app/src/lib/geochat/components/records-panel.svelte
@@ -104,7 +104,7 @@
           <p>{t.searchingMap}</p>
         </div>
       {:else if selectedRecord?.geometry}
-        {#key `${uuid}-${!!selectedRecord?.geometry}`}
+        {#key `${selectedRecord.uuid}-${!!selectedRecord.geometry}`}
           <Map {coordinates} id={selectedRecord.uuid} dynamic={true} mapType="record" mapVariant="geochat" footer={false} mapFill={true} />
         {/key}
       {:else}
